288e805caaa6e1ae51eb30699393e5a3615b507b
Alex Panic <gm4sp@mail.ru>
2023-11-12 15:50:52 +0500

    add iTunes API

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -5,18 +5,130 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
+
+    private val itunesBaseUrl = "https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(itunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val itunesService = retrofit.create(ItunesApi::class.java)
+    private var tracks = mutableListOf<Track>()
+
+    private lateinit var inputEditText: EditText
+    private lateinit var placeholderMessage: TextView
+    private lateinit var placeholderAlertIcon: ImageView
+    private lateinit var reSearchButton: Button
+    private lateinit var adapter: TrackAdapter
+
+    private fun showMessage(text: String, additionalMessage: String) {
+        if (text.isNotEmpty()) {
+            placeholderMessage.visibility = View.VISIBLE
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+            placeholderMessage.text = text
+            if (additionalMessage.isNotEmpty()) {
+                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                    .show()
+            }
+        } else {
+            placeholderMessage.visibility = View.GONE
+        }
+    }
+
+    private fun showAlertIcon(whatIcon: String?) {
+        reSearchButton.visibility = View.GONE
+        when (whatIcon) {
+            "nothing_found" -> {
+                placeholderAlertIcon.setImageResource(R.drawable.nothing_found)
+                placeholderAlertIcon.visibility = View.VISIBLE
+            }
+            "something_went_wrong" -> {
+                placeholderAlertIcon.setImageResource(R.drawable.something_went_wrong)
+                placeholderAlertIcon.visibility = View.VISIBLE
+                reSearchButton.visibility = View.VISIBLE
+            }
+            else -> {
+                placeholderAlertIcon.setImageResource(0)
+                placeholderAlertIcon.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun somethingWentWrong() {
+        showMessage(getString(R.string.something_went_wrong), "")
+        showAlertIcon("something_went_wrong")
+        hideKeyboard()
+    }
+
+    private fun findItunes() {
+        showAlertIcon(null)
+        showMessage("Ищем...", "")
+        itunesService.findTracks(inputEditText.text.toString())
+            .enqueue(object : Callback<FindTracksResponse> {
+                override fun onResponse(
+                    call: Call<FindTracksResponse>,
+                    response: Response<FindTracksResponse>
+                ) {
+                    when (response.code()) {
+                        200 -> {
+                            showAlertIcon(null)
+                            tracks.clear()
+                            if (response.body()?.results?.isNotEmpty() == true) {
+                                tracks.addAll(response.body()?.results!!)
+                                adapter.notifyDataSetChanged()
+                            } else {
+                                showMessage(getString(R.string.nothing_found), "")
+                                showAlertIcon("nothing_found")
+                            }
+                        }
+                        else -> {
+                            somethingWentWrong()
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<FindTracksResponse>, t: Throwable) {
+                    somethingWentWrong()
+                }
+            })
+    }
+
+    private fun hideKeyboard() {
+        val view: View? = this.currentFocus
+        if (view != null) {
+            val inputMethodManager =
+                getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
 
     private companion object {
         const val SEARCH_MASK = "SEARCH_MASK"
     }
-
     private var searchMask: String = ""
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -37,17 +149,28 @@ class SearchActivity : AppCompatActivity() {
             this.onBackPressed()
         }
 
-        val inputEditText = findViewById<EditText>(R.id.search_input)
+        placeholderMessage = findViewById(R.id.placeholderMessage)
+        placeholderAlertIcon = findViewById(R.id.placeholderAlertIcon)
+
+        inputEditText = findViewById<EditText>(R.id.search_input)
         inputEditText.setText(searchMask)
 
+        // кнопка очистки поисковой маски
         val clearButton = findViewById<ImageView>(R.id.clear_search_input)
         clearButton.setOnClickListener {
+            showMessage("","")
             inputEditText.setText("")
-            val view: View? = this.currentFocus
-            if (view != null) {
-                val inputMethodManager =
-                    getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+            hideKeyboard()
+        }
+
+        // кнопка перезапуска последнего поискового запроса
+        reSearchButton = findViewById<Button>(R.id.btnReloadSearch)
+        reSearchButton.setOnClickListener {
+            if (searchMask.isNotEmpty()) {
+                inputEditText.setText(searchMask)
+                findItunes()
             }
         }
 
@@ -71,29 +194,26 @@ class SearchActivity : AppCompatActivity() {
                 // empty
             }
         }
+
         inputEditText.addTextChangedListener(simpleTextWatcher)
 
-        // Список треков
-        val mockTracks = arrayListOf<Track>(
-            Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine","Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/CHECKPLACEHOLDERimage/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-        )
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (inputEditText.text.isNotEmpty()) {
+                    findItunes()
+                }
+            }
+            false
+        }
+
 
         val trackList = findViewById<RecyclerView>(R.id.trackList)
         trackList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        trackList.adapter = TrackAdapter(mockTracks)
+        adapter = TrackAdapter(tracks)
+        trackList.adapter = adapter
 
     }
 
-    private fun clearButtonVisibility(s: CharSequence?): Int {
-        return if (s.isNullOrEmpty()) {
-            View.GONE
-        } else {
-            View.VISIBLE
-        }
-    }
+
 
 }
