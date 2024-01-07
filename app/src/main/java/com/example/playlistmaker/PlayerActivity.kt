60af3ed38d87a85f03aaf567f0b6297618ea89c4
Alex Panic <gm4sp@mail.ru>
2024-01-07 17:11:46 +0500

    Отрывок трека можно играть на экране плеера

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -1,18 +1,42 @@
 package com.example.playlistmaker
 
+import android.media.MediaPlayer
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.MenuItem
+import android.view.WindowManager
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class PlayerActivity : AppCompatActivity() {
+
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+    }
+
+    private var playerState = STATE_DEFAULT
+
+    private lateinit var playButton: Button
+    private var mediaPlayer = MediaPlayer()
+    private var mainThreadHandler: Handler? = null
+    private var runnable: Runnable? = null
+    lateinit var trackPositionTimer: TextView
+
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +47,24 @@ class PlayerActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
 
+        trackPositionTimer = findViewById(R.id.trackPos)
+        playButton = findViewById(R.id.track_play_button)
 
         val track = intent.getSerializableExtra(Track.INTENT_EXTRA_ID, Track::class.java) as Track
+
+        mainThreadHandler = Handler(Looper.getMainLooper())
+        runnable = object : Runnable {
+            override fun run() {
+                refreshTimer()
+            }
+        }
+
+        playButton.setOnClickListener {
+            playbackControl()
+        }
+        preparePlayer(track.previewUrl)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         // увеличенная обложка
         val trackCover = findViewById<ImageView>(R.id.trackCover)
@@ -63,5 +103,76 @@ class PlayerActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+        releaseTimer()
+    }
+
+    private fun playbackControl() {
+        when(playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    private fun togglePlayButton(setAsPlay: Boolean) {
+        playButton.background = ContextCompat.getDrawable(applicationContext,
+            if (setAsPlay) R.drawable.play_btn else R.drawable.pause_btn)
+    }
+
+    private fun preparePlayer(trackUrl: String) {
+        mediaPlayer.setDataSource(trackUrl)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playButton.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            togglePlayButton(true)
+            playerState = STATE_PREPARED
+            releaseTimer()
+            trackPositionTimer.text = "00:00"
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        togglePlayButton(false)
+        playerState = STATE_PLAYING
+        mainThreadHandler?.post(runnable as Runnable)
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        togglePlayButton(true)
+        playerState = STATE_PAUSED
+        releaseTimer()
+    }
+
+    private fun releaseTimer() {
+        mainThreadHandler!!.removeCallbacks(runnable!!)
+    }
+
+    private fun refreshTimer() {
+        trackPositionTimer.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(mediaPlayer.currentPosition)
+        if (playerState == STATE_PLAYING) {
+            mainThreadHandler?.postDelayed(
+                runnable!!, 250
+            )
+        } else {
+            releaseTimer()
+        }
     }
 }
